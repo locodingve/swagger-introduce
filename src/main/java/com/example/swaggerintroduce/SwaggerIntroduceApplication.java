40a1c4d9f67c6d77e09f0a029744e87b37a52ab2
ellie <ellie@mkcreative.co.kr>
2022-12-08 17:37:01 +0900

    INIT

---
+++ src/main/java/com/example/swaggerintroduce/SwaggerIntroduceApplication.java
@@ -0,0 +1,13 @@
+package com.example.swaggerintroduce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerIntroduceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwaggerIntroduceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/swaggerintroduce/SwaggerIntroduceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swaggerintroduce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerIntroduceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
